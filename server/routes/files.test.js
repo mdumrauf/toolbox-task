1e8d92171ea6fd2b753dc14ad43bfe9a118b03ce
Matias Dumrauf <matias.dumrauf@gmail.com>
2023-05-26 02:29:06 -0300

    test(server): add test for GET /files/list

---
+++ server/routes/files.test.js
@@ -0,0 +1,23 @@
+const expect = require('chai').expect
+const nock = require('nock')
+const request = require('supertest')
+
+const app = require('../server')
+
+describe('Files', () => {
+  describe('GET /files/list', () => {
+    it('lists files exactly as external API does', async () => {
+      nock('https://echo-serv.tbxnet.com/v1/')
+        .get('/secret/files')
+        .reply(200, {
+          files: ['file1.csv', 'file2.csv', 'file42.csv']
+        })
+
+      const { body } = await request(app)
+        .get('/files/list')
+        .set('Accept', 'application/json')
+
+      expect(body).to.have.property('files').with.lengthOf(3)
+    })
+  })
+})
