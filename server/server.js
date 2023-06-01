d9e216f8a1b432b42f9b10c4aba69eacea017c3b
Matias Dumrauf <matias.dumrauf@gmail.com>
2023-05-31 22:52:21 -0300

    feat(server): use cors to allow fetching data from app's server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const { FilesRouter, HealthRouter } = require('./routes')
 
 const app = express()
 
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  optionsSuccessStatus: 200
+}
+
 app.use(bodyParser.json())
+app.use(cors(corsOptions))
 
 const router = express.Router()
 
